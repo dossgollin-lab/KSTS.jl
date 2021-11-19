328b97b6fb063dc4af6b4d2c6a33d5a06eab4b1e
sophiaprieto <sbp3@rice.edu>
2021-11-19 14:00:58 -0600

    Update util.jl

--- src/LDSSim/src/util.jl
+++ src/LDSSim/src/util.jl
@@ -6,3 +6,14 @@ $(SIGNATURES)
 function normalize(w::Vector{<:Real})
     return w ./ sum(w)
 end
+
+
+# +-30 days around current day every 12 months
+function season_window()
+    selected = zeros(6,61)
+    for y in 0:5
+        window = (n-30:n+30) .+ y*365
+        selected[y+1,:] = collect(window)
+    end
+    return selected
+end
